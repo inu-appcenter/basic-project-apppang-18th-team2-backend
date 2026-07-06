6d31b015ea52ad77a9b1d64f89518eb06e6ecaa5
suyeon <stellajeong6@naver.com>
2026-07-07 02:40:15 +0900

    ✨ Feature: 보안 설정을 위한 SecurityConfig 추가

---
+++ src/main/java/com/apppang/appgang2/global/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.apppang.appgang2.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    /* DB 연동 후 로직 수정
+    *  비밀번호 암호화
+    *  JWT 필터 추가 예정
+    * */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
