5444a51de2e880c4edda0b9885419940d5f78eb9
suyeon <stellajeong6@naver.com>
2026-07-13 13:27:08 +0900

    🔒security : 회원가입, 에러 페이지 API 접근 권한 허용 및 비밀번호 암호화

--- src/main/java/com/apppang/appgang2/global/config/SecurityConfig.java
+++ src/main/java/com/apppang/appgang2/global/config/SecurityConfig.java
@@ -4,21 +4,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     /* DB 연동 후 로직 수정
-    *  비밀번호 암호화
     *  JWT 필터 추가 예정
     * */
+
+    //비밀번호 암호화
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf(csrf -> csrf.disable())
+
+                //요청 권한 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login").permitAll()
+                        //모든 회원가입과 로그인은 허락
+                        .requestMatchers("/api/auth/signup","/api/auth/login","/error").permitAll()
+                        //그 외의 나머지 경로들은 모두 로그인해야 접근 가능
                         .anyRequest().authenticated()
                 );
         return http.build();
