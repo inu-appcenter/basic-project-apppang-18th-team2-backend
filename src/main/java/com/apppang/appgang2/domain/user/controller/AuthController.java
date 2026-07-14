1211c071857fa02a8dcb76b2512d9685790b462a
suyeon <stellajeong6@naver.com>
2026-07-14 18:34:54 +0900

    ♻️ refactor : 회원가입 응답 구조 개선 및 타입 변경

--- src/main/java/com/apppang/appgang2/domain/user/controller/AuthController.java
+++ src/main/java/com/apppang/appgang2/domain/user/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.apppang.appgang2.domain.user.controller;
 
 import com.apppang.appgang2.domain.user.dto.SignupDTO;
+import com.apppang.appgang2.domain.user.dto.SignupResponse;
 import com.apppang.appgang2.domain.user.service.AuthService;
 import com.apppang.appgang2.global.common.ApiResponse;
 import jakarta.validation.Valid;
@@ -12,9 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor    //final 필드에 대해 롬북이 생성자를 자동으로 만들어 의존성을 주입
 @RequestMapping("/api/auth")
@@ -24,19 +22,17 @@ public class AuthController {
 
     //회원가입
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<Map<String,Long>>> signup(@Valid @RequestBody SignupDTO signupDTO){
+    public ResponseEntity<ApiResponse<SignupResponse>> signup(@Valid @RequestBody SignupDTO signupDTO){
 
         //비즈니스 로직 완료 후 응답에 필요한 userId만 서비스로부터 받아옴
         Long savedUserId = authService.signup(signupDTO);
 
-        //API 명세서 "data" 구조를 맞추기 위해 반환받은 savedUserId를 userId 이름을 붙여 응답 객체 생성
-        Map<String, Long> data = new HashMap<>();
-        data.put("userId", savedUserId);
+        SignupResponse signupResponse = new SignupResponse(savedUserId);
 
         //201 Created 상태와 JSON 응답 반환
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(ApiResponse.success("회원가입이 완료되었습니다.",data));
+                .body(ApiResponse.success("회원가입이 완료되었습니다.",signupResponse));
     }
 
     //로그인, 따로 만들어뒀던 LoginController를 AuthController에 합칠 예정입니다.
