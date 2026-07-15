cbba4d9054cf79f4bb5595a18513c8f80a350918
김대건 <biggun0524@inu.ac.kr>
2026-07-15 16:54:06 +0900

    ⚙️ Setting : CI/CD 테스트

--- src/main/java/com/apppang/apppang2/Apppang2Application.java
+++ src/main/java/com/apppang/apppang2/Apppang2Application.java
@@ -1,13 +1,13 @@
-package com.apppang.appgang2;
+package com.apppang.apppang2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Appgang2Application {
+public class Apppang2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Appgang2Application.class, args);
+		SpringApplication.run(Apppang2Application.class, args);
 	}
 
 }
