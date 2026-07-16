66960b4c54cadce548e18e19ccff1c2fa79260f0
suyeon <stellajeong6@naver.com>
2026-07-17 00:14:17 +0900

    ✨ Feature: 로그인 응답 필터 구현

---
+++ src/main/java/com/apppang/apppang2/domain/user/dto/LoginResponse.java
@@ -0,0 +1,20 @@
+package com.apppang.apppang2.domain.user.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class LoginResponse {
+    private String accessToken;
+    private String refreshToken;
+    private UserInfo user;      //응답 데이터 구조의 user
+
+    @Getter
+    @Builder
+    public static class UserInfo{
+        private Long userId;
+        private String email;
+    }
+}
