52a6e87892a8654b79701785cf125c08033b1709
suyeon <stellajeong6@naver.com>
2026-07-17 10:22:00 +0900

    ✨ Feature :  배송지 추가 기능 리포지토리 추가

--- src/main/java/com/apppang/apppang2/domain/user/repository/UserRepository.java
+++ src/main/java/com/apppang/apppang2/domain/user/repository/UserRepository.java
@@ -12,6 +12,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     //가입된 유저인지 확인
     Optional<User> findByEmail(String email);
+
+    //AddressService에서 주소 추가를 위해 Id로 가입된 유저인지 확인
+    Optional<User> findById(Long userId);
+
     //이름과 전화번호가 일치하는 유저 찾기
     Optional<User> findByNameAndPhone(String name, String phone);
 }
