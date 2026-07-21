10a458d4124e4f6b58fb37398b742be0d8c31adb
suyeon <stellajeong6@naver.com>
2026-07-21 22:48:18 +0900

    ✨ Feature : 리뷰 수정

---
+++ src/main/java/com/apppang/apppang2/domain/review/dto/request/ReviewUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.apppang.apppang2.domain.review.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+
+@Getter
+public class ReviewUpdateRequest {
+    @NotNull(message = "별점은 필수입니다.")
+    @DecimalMin(value = "1.0")
+    @DecimalMax(value = "5.0")
+    private Double rating;
+
+    @NotBlank(message = "리뷰 내용을 입력해주세요.")
+    @Size(min = 10, max = 500, message = "리뷰는 10자 이상, 500자 이하로 작성해주세요")
+    private String content;
+}
