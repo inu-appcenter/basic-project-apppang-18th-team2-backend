0a24d2df978db748767dd3a203f7eab18310f385
suyeon <stellajeong6@naver.com>
2026-07-23 14:59:06 +0900

    ✨Feature: DTO 생성 방식 통일 및 토큰 재발급 응답 추가

--- src/main/java/com/apppang/apppang2/domain/auth/dto/response/EmailCheckResponse.java
+++ src/main/java/com/apppang/apppang2/domain/auth/dto/response/EmailCheckResponse.java
@@ -1,11 +1,10 @@
 package com.apppang.apppang2.domain.auth.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@Builder
 public class EmailCheckResponse {
-
-    private final boolean available;    //이메일 사용 가능 여부
+    private boolean available;    //이메일 사용 가능 여부
 }
