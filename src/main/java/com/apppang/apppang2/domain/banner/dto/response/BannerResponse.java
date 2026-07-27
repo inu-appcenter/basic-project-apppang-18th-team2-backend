fffe9065518578c08d1f6a29f464267b02c7381c
suyeon <stellajeong6@naver.com>
2026-07-27 15:15:58 +0900

    ♻️ Refactor :  응답 구조 수정

--- src/main/java/com/apppang/apppang2/domain/banner/dto/response/BannerResponse.java
+++ src/main/java/com/apppang/apppang2/domain/banner/dto/response/BannerResponse.java
@@ -1,7 +1,6 @@
 package com.apppang.apppang2.domain.banner.dto.response;
 
 import com.apppang.apppang2.domain.banner.entity.Banner;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -10,7 +9,7 @@ public class BannerResponse {
     private final String title;
     private final String imageUrl;
     private final String targetUrl;
-    
+
     //Banner 엔티티를 BannerResponse로 변환하기 위한 생성자
     public BannerResponse(Banner banner){
         this.bannerId = banner.getId();
