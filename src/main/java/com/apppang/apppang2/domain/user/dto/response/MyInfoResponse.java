303cea38c60e640b945cfd4187305098d6cfeaf5
suyeon <stellajeong6@naver.com>
2026-07-29 00:25:27 +0900

    ♻️ Refactor : @AllArgsConstructor를 @Builder로 변경

--- src/main/java/com/apppang/apppang2/domain/user/dto/response/MyInfoResponse.java
+++ src/main/java/com/apppang/apppang2/domain/user/dto/response/MyInfoResponse.java
@@ -1,21 +1,22 @@
 package com.apppang.apppang2.domain.user.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
-@Builder
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MyInfoResponse {
-
     private Long userId;
-
     private String email;
-
     private String name;
-
     private String phone;
-
     private String profileImage;
+
+    @Builder
+    public MyInfoResponse(Long userId, String email, String name, String phone, String profileImage){
+        this.userId = userId;
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        this.profileImage = profileImage;
+    }
 }
