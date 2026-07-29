9750a25ac9b87d17271aaa3fe5fa6704573e38ca
suyeon <stellajeong6@naver.com>
2026-07-29 11:42:23 +0900

    ♻️ Refactor : 활성화 여부 필드명 수정

--- src/main/java/com/apppang/apppang2/domain/banner/service/BannerService.java
+++ src/main/java/com/apppang/apppang2/domain/banner/service/BannerService.java
@@ -13,10 +13,9 @@ public class BannerService {
     private final BannerRepository bannerRepository;
 
     public List<BannerResponse> getActiveBanners(){
-        //bannerRepository에 있는 List<Banner>들이 나오고
-        return bannerRepository.findAllByIsActiveTrueOrderByDisplayOrderAsc()
-                .stream()   //여러 개의 Banner들이 흘러 map()을 통과해 BannerResponse로 변환됨
-                .map(banner -> new BannerResponse(banner))
+        return bannerRepository.findAllByActiveTrueOrderByDisplayOrderAsc()
+                .stream()
+                .map(BannerResponse::new)
                 .toList();  //변환된 BannerReponse들을 새로운 List에 담기
     }
 
